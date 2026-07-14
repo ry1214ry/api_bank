682a8466b74623debb4bb3527152edcb06cd467e
Your Name <your_email@example.com>
2026-07-14 16:10:03 +0700

    Update source code

--- src/main/java/com/example/authentication/config/WebConfig.java
+++ src/main/java/com/example/authentication/config/WebConfig.java
@@ -13,27 +13,25 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        // Popup Directory
+
         Path popupDir = Paths.get("./popup");
         registry.addResourceHandler("/images/popup/**")
                 .addResourceLocations(popupDir.toAbsolutePath().toUri().toString());
 
-        // Products Directory
         Path productsDir = Paths.get("./images/products");
         registry.addResourceHandler("/images/products/**")
                 .addResourceLocations(productsDir.toAbsolutePath().toUri().toString());
 
-        // Promotions Directory
         Path promoDir = Paths.get("./images/promotions");
         registry.addResourceHandler("/images/promotions/**")
                 .addResourceLocations(promoDir.toAbsolutePath().toUri().toString());
 
-        // Contacts Directory
+
         Path contactDir = Paths.get("./images/contacts");
         registry.addResourceHandler("/images/contacts/**")
                 .addResourceLocations(contactDir.toAbsolutePath().toUri().toString());
 
-        // News Directory (Fixed duplicate variables here)
+
         Path newsDir = Paths.get("./images/news");
         registry.addResourceHandler("/images/news/**")
                 .addResourceLocations(newsDir.toAbsolutePath().toUri().toString());
